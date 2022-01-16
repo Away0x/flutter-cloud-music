fb74441bbf51f840324d81a106d4eae5ed8848c8
Tong Wu <wutong0910@foxmail.com>
2022-01-16 15:49:15 +0800

    add tabbar

--- lib/pages/recommend/view.dart
+++ lib/pages/recommend/view.dart
@@ -2,8 +2,22 @@ import 'package:cloudmusic/config/app.dart';
 import 'package:cloudmusic/config/theme.dart';
 import 'package:flutter/material.dart';
 
-class RecommendPage extends StatelessWidget {
+class RecommendPage extends StatefulWidget {
   const RecommendPage({Key? key}) : super(key: key);
+
+  @override
+  _RecommendPageState createState() => _RecommendPageState();
+}
+
+class _RecommendPageState extends State<RecommendPage>
+    with TickerProviderStateMixin {
+  late final TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +25,17 @@ class RecommendPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(AppInfo.title),
         backgroundColor: AppTheme.primary,
+        bottom: TabBar(
+          controller: tabController,
+          tabs: const [
+            Text('推荐'),
+            Text('歌手'),
+            Text('排行榜'),
+          ],
+        ),
       ),
-      drawer: Container(
-        color: Colors.white,
-        child: const Text('drawer'),
+      drawer: const Drawer(
+        child: Text('drawer'),
       ),
       body: const Text('RecommendPage'),
     );
